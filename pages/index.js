2c4152c0f7b1cff78bf41d9a29313da8dbdbdf53
RB <87038824+MrMhirs@users.noreply.github.com>
2023-01-05 17:22:44 +0100

    Card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import Head from 'next/head';
-
-
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+import Image from "next/image";
+import devrob from "../public/dev-rob-wave.png";
+import design from "../public/design.png"
+import code from "../public/code.png"
+import consulting from "../public/consulting.png"
 
 export default function Home() {
   return (
@@ -11,9 +20,69 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
 
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">copiedbyrob</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Robert Baguena
+            </h2>
+            <h3 className="text-2xl py-2">Developer and designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Junior developer eager to learn and to work with the best
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillGithub />
+            <AiFillLinkedin />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={devrob} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+        <section>
+        <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a developer i have acquired
+              <span className="text-teal-500"> the passion </span>
+              for <span className="text-teal-500">self-development </span>
+              in order to acquired <span className="text-teal-500"> the knowledge </span> to have a <span className="text-teal-500"> dedication</span> on future opportunities.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer i wide range of services, including programming, tech-support and designing.
+            </p>
+          </div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={design} width={100} height={100} />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs following core design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools i use</h4>
+            <p className="text-gray-800 py-1">Photoshop</p>
+            <p className="text-gray-800 py-1">Illustrator</p>
+            <p className="text-gray-800 py-1">Figma</p>
+          </div>
+        </section>
       </main>
     </>
-  )
+  );
 }
